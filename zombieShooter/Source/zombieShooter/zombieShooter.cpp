d191fe038729c8da2c09611d53e039a3f68f5c7e
BarrieDoornbos <barriedoornbos@hotmail.com>
2026-04-01 09:43:51 +0200

    Added the Unreal project

---
+++ zombieShooter/Source/zombieShooter/zombieShooter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "zombieShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, zombieShooter, "zombieShooter" );
